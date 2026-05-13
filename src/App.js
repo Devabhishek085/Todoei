53752470668f5e094d81c0342fd3474961e5f2db
Abhishek Pal <palabhishek085@gmail.com>
2026-05-13 18:15:16 +0530

    todo_style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import ToDoItem from './components/todoitem';
+import Button from './components/Button';
 
 const App = ()=>{
   return (
-    <div>
+    <div class="todo-container">
       <Header/>
       <ToDoItem/>
+      <ToDoItem/>
+      <ToDoItem/>
+      <ToDoItem/>
+      <Button/>
     </div>
   )
 }
