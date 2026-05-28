2ae56a87bb0b58ce3a7b39ba3e15462e00f8a54d
Abhishek Pal <palabhishek085@gmail.com>
2026-05-28 23:36:23 +0530

    Added  a TodoItem

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Header from './components/header';
 import ToDoItem from './components/todoitem';
 import Button from './components/Button';
+import CounterComponent from './components/CounterComponent';
 
 const App = ()=>{
   return (
     <div class="todo-container">
+      <CounterComponent/>
       <Header title="This is a TodoApp"/>
+      <ToDoItem text="Protien"/>
       <ToDoItem text="Eat"/>
       <ToDoItem text="Code"/>
       <ToDoItem text="Sleep"/>
