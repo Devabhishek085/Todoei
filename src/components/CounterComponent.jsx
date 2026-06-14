2cfb501dc27383cfebf1cedec5d57015b51e4b81
Abhishek Pal <palabhishek085@gmail.com>
2026-06-14 12:58:06 +0530

    Implementing useState hook

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 const CounterComponent=() => {
-    const val=useState(0);
-    console.log(val);
-    
+    const [count,setCount]=useState(0);
+
     return(
         <div>
             <p>
-                Count Component
+                Count Component - {count}
             </p>
+            <h3>The no is {count%2===0?"EVEN":"ODD"}</h3>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count-1)}>Decrement</button>
         </div>
     )
 }
